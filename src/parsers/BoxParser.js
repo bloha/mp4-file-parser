e96f3218ddf5ad7c31fd8c2a774154f0e898b0e9
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-06-19 21:18:39 +0300

    Replace header parser with BoxParser class

---
+++ src/parsers/BoxParser.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import { ExecutionSequence } from '../sequence/ExecutionSequence.js';
+
+export class BoxParser {
+
+    constructor({ blob, offset }) {
+        this.sequence = new ExecutionSequence({ blob, offset });
+        this.sequence.add('size', async (parser) => { return await parser.takeUint32(); });
+        this.sequence.add('type', async (parser) => { return await parser.takeText(4); });
+        this.sequence.add('largeSize', async (parser) => {
+            const size = parser.getField('size');
+            if (size === 1) {
+                return await parser.takeUint64();
+            }
+            if (size === 0) {
+                return parser.getBlob().size - parser.getBoxOffset();
+            }
+        });
+        this.sequence.add('userType', async (parser) => {
+            if (parser.getField('type') === 'uuid') {
+                return await parser.takeText(16);
+            }
+        });
+    }
+
+    async parse() {
+        return await this.sequence.execute();
+    }
+
+}
