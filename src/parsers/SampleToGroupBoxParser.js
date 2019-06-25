a869ab84c634284d51464f65e959f1447c2c7be9
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-06-25 19:40:17 +0300

    Refactor sbgp parser class

--- src/parsers/SampleToGroupBoxParser.js
+++ src/parsers/SampleToGroupBoxParser.js
@@ -7,7 +7,10 @@ export class SampleToGroupBoxParser extends FullBoxParser {
 
     constructor({ blob, offset }) {
         super({ blob, offset });
-        this.sequence.add({ name: 'grouping_type', method: Parser.parseUint32 });
+        this.sequence.add({
+            name: 'grouping_type',
+            method: Parser.parseUint32
+        });
         this.sequence.add({
             name: 'grouping_type_parameter',
             method: Parser.parseIfVersionEquals,
@@ -16,18 +19,25 @@ export class SampleToGroupBoxParser extends FullBoxParser {
                 version: 1
             }
         });
-        this.sequence.add({ name: 'entry_count', method: Parser.parseUint32 });
+        this.sequence.add({
+            name: 'entry_count',
+            method: Parser.parseUint32
+        });
         this.sequence.add({
             name: 'entries',
-            method: async (parser) => {
-                const entries = [];
-                for (let i = 0; i < parser.getField('entry_count'); i++) {
-                    const entry = new Map();
-                    entry.set('sample_count', await parser.takeUint32());
-                    entry.set('group_description_index', await parser.takeUint32());
-                    entries.push(entry);
-                }
-                return entries;
+            method: Parser.parseEntries,
+            parameters: {
+                amount: 'entry_count',
+                fields: [
+                    {
+                        name: 'sample_count',
+                        method: Parser.parseUint32
+                    },
+                    {
+                        name: 'group_description_index',
+                        method: Parser.parseUint32
+                    }
+                ]
             }
         });
     }
