08be22953cb744ef347bb214a9c3e99741a1aa99
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-06-26 13:35:01 +0300

    Refactor trun parser class

--- src/parsers/TrackRunBoxParser.js
+++ src/parsers/TrackRunBoxParser.js
@@ -7,48 +7,68 @@ export class TrackRunBoxParser extends FullBoxParser {
 
     constructor({ blob, offset }) {
         super({ blob, offset });
-        this.sequence.add({ name: 'sample_count', method: Parser.parseUint32 });
+        this.sequence.add({
+            name: 'sample_count',
+            method: Parser.parseUint32
+        });
         this.sequence.add({
             name: 'data_offset',
             method: Parser.parseIfBoxHasFlags,
             parameters: {
-                method: Parser.parseInt32,
-                flags: 0x000001
+                flags: 0x000001,
+                method: Parser.parseInt32
             }
         });
         this.sequence.add({
             name: 'first_sample_flags',
             method: Parser.parseIfBoxHasFlags,
             parameters: {
-                method: Parser.parseUint32,
-                flags: 0x000004
+                flags: 0x000004,
+                method: Parser.parseUint32
             }
         });
         this.sequence.add({
             name: 'entries',
-            method: async (parser) => {
-                const entries = [];
-                for (let i = 0; i < parser.getField('sample_count'); i++) {
-                    const entry = new Map();
-                    if (parser.boxHasFlags(0x000100)) {
-                        entry.set('sample_duration', await parser.takeUint32());
-                    }
-                    if (parser.boxHasFlags(0x000200)) {
-                        entry.set('sample_size', await parser.takeUint32());
-                    }
-                    if (parser.boxHasFlags(0x000400)) {
-                        entry.set('sample_flags', await parser.takeUint32());
-                    }
-                    if (parser.boxHasFlags(0x000800)) {
-                        if (parser.getField('version') === 0) {
-                            entry.set('sample_composition_time_offset', await parser.takeUint32());
-                        } else {
-                            entry.set('sample_composition_time_offset', await parser.takeInt32());
+            method: Parser.parseEntries,
+            parameters: {
+                amount: 'sample_count',
+                fields: [
+                    {
+                        name: 'sample_duration',
+                        method: Parser.parseIfBoxHasFlags,
+                        parameters: {
+                            flags: 0x000100,
+                            method: Parser.parseUint32
+                        }
+                    },
+                    {
+                        name: 'sample_size',
+                        method: Parser.parseIfBoxHasFlags,
+                        parameters: {
+                            flags: 0x000200,
+                            method: Parser.parseUint32
+                        }
+                    },
+                    {
+                        name: 'sample_flags',
+                        method: Parser.parseIfBoxHasFlags,
+                        parameters: {
+                            flags: 0x000400,
+                            method: Parser.parseUint32
+                        }
+                    },
+                    {
+                        name: 'sample_composition_time_offset',
+                        method: Parser.parseIfBoxHasFlags,
+                        parameters: {
+                            flags: 0x000800,
+                            method: Parser.parseByVersion,
+                            parameters: {
+                                methods: [Parser.parseUint32, Parser.parseInt32]
+                            }
                         }
                     }
-                    entries.push(entry);
-                }
-                return entries;
+                ]
             }
         });
     }
