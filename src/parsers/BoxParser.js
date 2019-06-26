6f18216aad3f0adc5da6c84a26af83f4389e38fb
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-06-26 19:08:49 +0300

    Refactor BoxParser class

--- src/parsers/BoxParser.js
+++ src/parsers/BoxParser.js
@@ -7,7 +7,10 @@ export class BoxParser {
 
     constructor({ blob, offset }) {
         this.sequence = new ExecutionSequence({ blob, offset });
-        this.sequence.add({ name: 'size', method: Parser.parseUint32 });
+        this.sequence.add({
+            name: 'size',
+            method: Parser.parseUint32
+        });
         this.sequence.add({
             name: 'type',
             method: Parser.parseText,
@@ -30,9 +33,13 @@ export class BoxParser {
         });
         this.sequence.add({
             name: 'usertype',
-            method: async (parser) => {
-                if (parser.getField('type') === 'uuid') {
-                    return await parser.takeText(16);
+            method: Parser.parseByCondition,
+            parameters: {
+                condition: (value) => (value === 'uuid'),
+                values: ['type'],
+                method: Parser.parseText,
+                parameters: {
+                    amount: 16
                 }
             }
         });
