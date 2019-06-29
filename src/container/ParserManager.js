423c10a8d97d57aeec0c3706c04a261d7ee2edd1
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-06-29 15:34:50 +0300

    Implement DataEntryUrlBoxParser class

--- src/container/ParserManager.js
+++ src/container/ParserManager.js
@@ -11,6 +11,7 @@ import { CompositionToDecodeBoxParser } from '../parsers/CompositionToDecodeBoxP
 import { ContainerBoxParser } from '../parsers/ContainerBoxParser.js';
 import { EditListBoxParser } from '../parsers/EditListBoxParser.js';
 import { CopyrightBoxParser } from '../parsers/CopyrightBoxParser.js';
+import { DataEntryUrlBoxParser } from '../parsers/DataEntryUrlBoxParser.js';
 import { DataReferenceBoxParser } from '../parsers/DataReferenceBoxParser.js';
 import { ExtendedLanguageBoxParser } from '../parsers/ExtendedLanguageBoxParser.js';
 import { FdItemInformationBoxParser } from '../parsers/FdItemInformationBoxParser.js';
@@ -79,6 +80,7 @@ export class ParserManager {
         this.parsers.set('ctts', CompositionOffsetBoxParser);
         this.parsers.set('cslg', CompositionToDecodeBoxParser);
         this.parsers.set('cprt', CopyrightBoxParser);
+        this.parsers.set('url ', DataEntryUrlBoxParser);
         this.parsers.set('dref', DataReferenceBoxParser);
         this.parsers.set('elst', EditListBoxParser);
         this.parsers.set('elng', ExtendedLanguageBoxParser);
