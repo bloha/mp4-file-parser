6387851333b82a490cd03cec21ec78501150e7fe
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-07-08 16:56:48 +0300

    Rename the abstract methods of the DataParser

--- src/sequence/parser/data/DataParser.js
+++ src/sequence/parser/data/DataParser.js
@@ -19,102 +19,102 @@ export class DataParser {
     }
 
     async takeInt8() {
-        const value = await this._takeInt8();
+        const value = await this._getInt8();
         this.head.move(1);
         return value;
     }
 
     async takeUint8() {
-        const value = await this._takeUint8();
+        const value = await this._getUint8();
         this.head.move(1);
         return value;
     }
 
     async takeInt16() {
-        const value = await this._takeInt16();
+        const value = await this._getInt16();
         this.head.move(2);
         return value;
     }
 
     async takeUint16() {
-        const value = await this._takeUint16();
+        const value = await this._getUint16();
         this.head.move(2);
         return value;
     }
 
     async takeInt32() {
-        const value = await this._takeInt32();
+        const value = await this._getInt32();
         this.head.move(4);
         return value;
     }
 
     async takeUint32() {
-        const value = await this._takeUint32();
+        const value = await this._getUint32();
         this.head.move(4);
         return value;
     }
 
     async takeInt64() {
-        const value = await this._takeInt64();
+        const value = await this._getInt64();
         this.head.move(8);
         return value;
     }
 
     async takeUint64() {
-        const value = await this._takeUint64();
+        const value = await this._getUint64();
         this.head.move(8);
         return value;
     }
 
     async takeText(size) {
-        const value = await this._takeText(size);
+        const value = await this._getText(size);
         this.head.move(size);
         return value;
     }
 
     async takeBuffer(size) {
-        const value = await this._takeBuffer(size);
+        const value = await this._getBuffer(size);
         this.head.move(size);
         return value;
     }
 
-    async _takeInt8() {
+    async _getInt8() {
         Abstraction.needsImplementation();
     }
 
-    async _takeUint8() {
+    async _getUint8() {
         Abstraction.needsImplementation();
     }
 
-    async _takeInt16() {
+    async _getInt16() {
         Abstraction.needsImplementation();
     }
 
-    async _takeUint16() {
+    async _getUint16() {
         Abstraction.needsImplementation();
     }
 
-    async _takeInt32() {
+    async _getInt32() {
         Abstraction.needsImplementation();
     }
 
-    async _takeUint32() {
+    async _getUint32() {
         Abstraction.needsImplementation();
     }
 
-    async _takeInt64() {
+    async _getInt64() {
         Abstraction.needsImplementation();
     }
 
-    async _takeUint64() {
+    async _getUint64() {
         Abstraction.needsImplementation();
     }
 
-    async _takeText(size) {
+    async _getText(size) {
         Abstraction.needsImplementation();
     }
 
-    async _takeBuffer(size) {
+    async _getBuffer(size) {
         Abstraction.needsImplementation();
     }
 
--- src/sequence/parser/data/blob/BlobParser.js
+++ src/sequence/parser/data/blob/BlobParser.js
@@ -13,61 +13,61 @@ export class BlobParser extends DataParser {
         this.head.move(amount);
     }
 
-    async _takeInt8() {
+    async _getInt8() {
         const buffer = await this._fetchBuffer(1);
         const value = new DataView(buffer).getInt8(0);
         return value;
     }
 
-    async _takeUint8() {
+    async _getUint8() {
         const buffer = await this._fetchBuffer(1);
         const value = new DataView(buffer).getUint8(0);
         return value;
     }
 
-    async _takeInt16() {
+    async _getInt16() {
         const buffer = await this._fetchBuffer(2);
         const value = new DataView(buffer).getInt16(0);
         return value;
     }
 
-    async _takeUint16() {
+    async _getUint16() {
         const buffer = await this._fetchBuffer(2);
         const value = new DataView(buffer).getUint16(0);
         return value;
     }
 
-    async _takeInt32() {
+    async _getInt32() {
         const buffer = await this._fetchBuffer(4);
         const value = new DataView(buffer).getInt32(0);
         return value;
     }
 
-    async _takeUint32() {
+    async _getUint32() {
         const buffer = await this._fetchBuffer(4);
         const value = new DataView(buffer).getUint32(0);
         return value;
     }
 
-    async _takeInt64() {
+    async _getInt64() {
         const buffer = await this._fetchBuffer(8);
         const value = new DataView(buffer).getBigInt64(0);
         return value;
     }
 
-    async _takeUint64() {
+    async _getUint64() {
         const buffer = await this._fetchBuffer(8);
         const value = new DataView(buffer).getBigUint64(0);
         return value;
     }
 
-    async _takeText(size) {
+    async _getText(size) {
         const buffer = await this._fetchBuffer(size);
         const text = new TextDecoder().decode(buffer);
         return text;
     }
 
-    async _takeBuffer(size) {
+    async _getBuffer(size) {
         const buffer = await this._fetchBuffer(size);
         return buffer;
     }
