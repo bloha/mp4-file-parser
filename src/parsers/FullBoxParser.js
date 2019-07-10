d529416b3ef001fe4c47d19e2c8bf9761a4bf7ec
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-07-10 23:15:35 +0300

    Adapt the FullBoxParser class

--- src/parsers/FullBoxParser.js
+++ src/parsers/FullBoxParser.js
@@ -5,23 +5,28 @@ import { Parser } from '../sequence/parser/Parser.js';
 
 export class FullBoxParser extends BoxParser {
 
-    constructor({ blob, offset }) {
-        super({ blob, offset });
-        this.sequence.add({ name: 'version', method: Parser.parseUint8 });
-        this.sequence.add({
-            name: 'flags',
-            method: async (parser) => {
-                const buffer = await parser.takeBuffer(3);
-                const view = new DataView(buffer);
-                const extendedBuffer = new ArrayBuffer(4);
-                const extendedView = new DataView(extendedBuffer);
-                extendedView.setUint8(0, 0);
-                extendedView.setInt8(1, view.getUint8(0));
-                extendedView.setInt8(2, view.getUint8(1));
-                extendedView.setInt8(3, view.getUint8(2));
-                return extendedView.getUint32(0);
+    getLogicBlocks() {
+        return [
+            ...super.getLogicBlocks(),
+            {
+                name: 'version',
+                method: Parser.parseUint8
+            },
+            {
+                method: async ({ entityParser }) => {
+                    const buffer = await entityParser.getDataParser().takeBuffer(3);
+                    const view = new DataView(buffer);
+                    const extendedBuffer = new ArrayBuffer(4);
+                    const extendedView = new DataView(extendedBuffer);
+                    extendedView.setUint8(0, 0);
+                    extendedView.setInt8(1, view.getUint8(0));
+                    extendedView.setInt8(2, view.getUint8(1));
+                    extendedView.setInt8(3, view.getUint8(2));
+                    const flags = extendedView.getUint32(0);
+                    entityParser.addField('flags', flags);
+                }
             }
-        });
+        ];
     }
 
 }
