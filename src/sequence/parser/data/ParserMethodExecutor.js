13f96fd4ae5331df92330a997e7c502932b87bf2
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-07-11 16:27:15 +0300

    Make the ParserMethodExecutor class abstract

--- src/sequence/parser/data/ParserMethodExecutor.js
+++ src/sequence/parser/data/ParserMethodExecutor.js
@@ -1,10 +1,17 @@
 'use strict';
 
+import { Abstraction } from '../../../../modules/javascript-abstraction/src/Abstraction.js';
 import { ValueExtractor } from '../value/ValueExtractor.js';
 
+/**
+ * Abstract Class ParserMethodExecutor.
+ * 
+ * @class ParserMethodExecutor
+ */
 export class ParserMethodExecutor {
 
     constructor({ entityParser, logicBlock }) {
+        Abstraction.needsInheritance(new.target, ParserMethodExecutor);
         this.entityParser = entityParser;
         this.logicBlock = logicBlock;
     }
