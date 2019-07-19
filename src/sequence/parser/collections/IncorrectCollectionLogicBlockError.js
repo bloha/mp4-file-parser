fba86b5e2fc1ea7d148aff260f7937812d0edf7c
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-07-19 14:45:07 +0300

    Remove legacy code

--- src/sequence/parser/collections/IncorrectCollectionLogicBlockError.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export class IncorrectCollectionLogicBlockError extends Error {
-
-    constructor() {
-        super(`Collection logic block must contain 'amount' or 'while' field.`);
-    }
-
-}
