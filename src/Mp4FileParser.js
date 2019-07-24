178c51e7ce637aaf075fce3ecf35929ebd3d3f89
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-07-24 14:44:56 +0300

    Accept the array buffer as data

--- src/Mp4FileParser.js
+++ src/Mp4FileParser.js
@@ -2,16 +2,17 @@
 
 import { BlobParser } from './data/blob/BlobParser.js';
 import { MainParser } from './parsers/MainParser.js';
+import { BufferParser } from './data/buffer/BufferParser.js';
 
 export class Mp4FileParser {
 
-    constructor(blob) {
-        this.blob = blob;
+    constructor(data) {
+        const dataParserClass = data instanceof Blob ? BlobParser : BufferParser;
+        this.dataParser = new dataParserClass({ data, offset: 0 });
     }
 
     async parse() {
-        const dataParser = new BlobParser({ data: this.blob, offset: 0 });
-        const parser = new MainParser({ dataParser });
+        const parser = new MainParser({ dataParser: this.dataParser });
         await parser.parse();
         const children = parser.getRootEntry().get('children');
         return children;
