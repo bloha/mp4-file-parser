a4f1c6218225018c8a82e2ca1e4f615481fef770
Andrei Šukurov <14295673+bloha@users.noreply.github.com>
2019-09-19 17:04:24 +0300

    Stop reading UTF-16 strings as UTF-8

--- src/logic/string/ByteOrderMarkStringLogicBlockBuilder.js
+++ src/logic/string/ByteOrderMarkStringLogicBlockBuilder.js
@@ -1,7 +1,20 @@
 'use strict';
 
 import { StringLogicBlockBuilder } from './StringLogicBlockBuilder.js';
+import { ByteOrderMarkStringLogicBlock } from './ByteOrderMarkStringLogicBlock.js';
 
 export class ByteOrderMarkStringLogicBlockBuilder extends StringLogicBlockBuilder {
+
+    build() {
+        return new ByteOrderMarkStringLogicBlock({
+            entityParser: this.entityParser,
+            name: this.name,
+            type: this.type,
+            versions: this.versions,
+            flags: this.flags,
+            conditions: this.conditions,
+            elseLogicBlock: this.elseLogicBlock,
+        });
+    }
 
 }
